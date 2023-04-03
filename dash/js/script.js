d338a0bfe34a3355a10a2d0e9348ee77afaf59a0
Delfino Ruiz <druiz@quimica-apollo.com.mx>
2023-04-03 17:25:26 -0600

    speed slider

--- dash/js/script.js
+++ dash/js/script.js
@@ -568,7 +568,7 @@
 					thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
 					selector:  "[data-lightgallery='item']",
 					autoplay:  $(itemsToInit).attr("data-lg-autoplay") === "true",
-					pause:     parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 2000,
+					pause:     parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 1900,
 					addClass:  addClass,
 					mode:      $(itemsToInit).attr("data-lg-animation") || "lg-slide",
 					loop:      $(itemsToInit).attr("data-lg-loop") !== "false"
@@ -588,7 +588,7 @@
 						thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
 						selector:  "[data-lightgallery='item']",
 						autoplay:  $(itemsToInit).attr("data-lg-autoplay") === "true",
-						pause:     parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 2000,
+						pause:     parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 1900,
 						addClass:  addClass,
 						mode:      $(itemsToInit).attr("data-lg-animation") || "lg-slide",
 						loop:      $(itemsToInit).attr("data-lg-loop") !== "false",
