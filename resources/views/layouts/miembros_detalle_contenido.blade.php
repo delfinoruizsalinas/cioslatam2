58cfb86b7a35969c4556fe710156d4d49d93a3d5
Delfino Ruiz <druiz@quimica-apollo.com.mx>
2023-05-31 11:32:15 -0600

    icon share

--- resources/views/layouts/miembros_detalle_contenido.blade.php
+++ resources/views/layouts/miembros_detalle_contenido.blade.php
@@ -5,11 +5,20 @@
     <title>{{  $title }}</title>
     @include('layouts.css')
     <style>
-      @media (max-width: 400px) {
+      @media (max-width: 600px) {
         #portada {
             background-image: url(/dash/images/cios/pexels-andrea-piacquadio-789822_mobile.jpg) !important;
             background-attachment: unset;
         }
+        .share {
+            right: 0;
+            top: 50%;
+            z-index: 2;
+        }
+        .fixed{
+          position: fixed;
+        }
+        
       }
       .sizeImage{
         padding: 0.25rem;
@@ -33,6 +42,7 @@
           color: #222;
           background-color: #ccc;
       }
+
       </style>
 
   </head>
@@ -70,26 +80,30 @@
             <div class="col-lg-6 col-xl-5">
               <ul class="blog-post-meta">
                 <li>
-                  <span class="icon mdi mdi-clock"></span>
+                  <span class="icon mdi mdi-calendar"></span>
                   {{ \Carbon\Carbon::parse($detalle_contenido[0]->updated_at)->translatedFormat('d F, Y') }}
                 </li>
+              </ul>
+
+              <ul class="blog-post-meta">
                 <li>
+                  <h6>Por {{ $detalle_contenido[0]->nom_contacto }} {{ $detalle_contenido[0]->ap_contacto }}</h6>
+                </li>
+                <li class="float-right">
+                  <div class="share md:flex fixed md:static">
+                    <div onclick="javascript:url=window.document.URL;title=window.document.title;let shareDat={title,url};navigator.share(shareDat)" title="Compartir enlace" style="float:left;height: 32px;width: 32px;cursor:pointer;"><svg xmlns="https://cioslatam.com/" width="32" height="32" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><path d="M785.8,663.3c-45.4,0-90.7,18.1-118,45.4L368.4,536.3c0-9.1,9.1-27.2,9.1-36.3c0-18.1,0-27.2-9.1-36.3l308.5-172.4c27.2,27.2,72.6,45.4,118,45.4c90.7,0,163.3-72.6,163.3-163.3C958.2,82.6,885.6,10,794.9,10c-90.7,0-172.4,63.5-172.4,154.3c0,18.1,0,27.2,9.1,36.3L323.1,382c-36.3-27.2-72.6-45.4-118-45.4c-90.7,0-163.3,72.6-163.3,163.3c0,90.7,72.6,163.3,163.3,163.3c45.4,0,90.7-18.2,118-45.4l308.5,172.4c0,9.1-9.1,27.2-9.1,36.3c0,90.7,72.6,163.3,163.3,163.3s163.3-72.6,163.3-163.3C949.2,735.9,876.6,663.3,785.8,663.3z"></path></g></svg></div>
+                  </div>
                 </li>
               </ul>
-              <h6>Por {{ $detalle_contenido[0]->nom_contacto }} {{ $detalle_contenido[0]->ap_contacto }}</h6>
+              
 
+              
               <h3 class="blog-post-title"> {{ $detalle_contenido[0]->titulo }}</h3>
               <div class="blog-post-content">
               {!! html_entity_decode($detalle_contenido[0]->resumen) !!}  
               </div>
             </div>
             <div class="col-lg-6 col-xl-5"><img src="/news/{{$detalle_contenido[0]->imagen}}" alt="" width="518" height="569"/>
-            </div>
-          </div>
-          
-          <div class="row">
-            <div class="col-12">
-              <div onclick="javascript:url=window.document.URL;title=window.document.title;let shareDat={title,url};navigator.share(shareDat)" title="Compartir enlace" style="float:left;height: 32px;width: 32px;margin:12px 6px 2px 12px;cursor:pointer;"><svg xmlns="https://cioslatam.com/" width="32" height="32" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><path d="M785.8,663.3c-45.4,0-90.7,18.1-118,45.4L368.4,536.3c0-9.1,9.1-27.2,9.1-36.3c0-18.1,0-27.2-9.1-36.3l308.5-172.4c27.2,27.2,72.6,45.4,118,45.4c90.7,0,163.3-72.6,163.3-163.3C958.2,82.6,885.6,10,794.9,10c-90.7,0-172.4,63.5-172.4,154.3c0,18.1,0,27.2,9.1,36.3L323.1,382c-36.3-27.2-72.6-45.4-118-45.4c-90.7,0-163.3,72.6-163.3,163.3c0,90.7,72.6,163.3,163.3,163.3c45.4,0,90.7-18.2,118-45.4l308.5,172.4c0,9.1-9.1,27.2-9.1,36.3c0,90.7,72.6,163.3,163.3,163.3s163.3-72.6,163.3-163.3C949.2,735.9,876.6,663.3,785.8,663.3z"></path></g></svg></div>
             </div>
           </div>
 
