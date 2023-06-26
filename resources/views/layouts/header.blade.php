e66597e30a083f75e3588bbfd7de1f8040012e27
Delfino Ruiz <druiz@quimica-apollo.com.mx>
2023-06-26 14:19:29 -0600

    tag analytics

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -83,4 +83,13 @@
         </nav>
     </div>
 </header>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5YSQ82VP1J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5YSQ82VP1J');
+</script>
 
