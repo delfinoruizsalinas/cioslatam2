9d9657ce57a7b33c7be313e5b68b000079a8617f
Delfino Ruiz Salinas <delfino.ruiz@origon.io>
2024-08-06 19:00:41 -0600

    unpublished

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -16,143 +16,146 @@ class AgendaController extends Controller
             // Decode the JSON string into an object
             $obj = json_decode($json);
 
-           
-            // In the case of this input, do key and array lookups to get the values
-            foreach ($obj->data as $key => $value) {
-             
-                $sep7 = null;
-                $sep8 = null;
-                $sep9 = null;
-                $sep10 = null;
-                $dia7_file = null;
-                $dia8_file = null;
-                $dia9_file = null;
-
-                $dia7_clase ='bi-red';
-                $dia8_clase ='bi-red';
-                $dia9_clase ='bi-red';
+            if(count($obj->data) > 0){
+                            // In the case of this input, do key and array lookups to get the values
+                foreach ($obj->data as $key => $value) {
                 
-                if(empty($value->attributes->SEP7)){
-                    $sep7 = '';
-                }else{
-                    $sep7 = $value->attributes->SEP7;                        
-                }
+                    $sep7 = null;
+                    $sep8 = null;
+                    $sep9 = null;
+                    $sep10 = null;
+                    $dia7_file = null;
+                    $dia8_file = null;
+                    $dia9_file = null;
 
-                if(empty($value->attributes->SEP8)){
-                    $sep8 = '';
-                }else{
-                    $sep8 = $value->attributes->SEP8;                        
-                }
-                if(empty($value->attributes->SEP9)){
-                    $sep9 = '';
-                }else{
-                    $sep9 = $value->attributes->SEP9;                        
-                }
-                if(empty($value->attributes->SEP10)){
-                    $sep10 = '';
-                }else{
-                    $sep10 = $value->attributes->SEP10;                        
-                }   
+                    $dia7_clase ='bi-red';
+                    $dia8_clase ='bi-red';
+                    $dia9_clase ='bi-red';
+                    
+                    if(empty($value->attributes->SEP7)){
+                        $sep7 = '';
+                    }else{
+                        $sep7 = $value->attributes->SEP7;                        
+                    }
 
-                if(empty($value->attributes->MESA_DIA_7->data->attributes->url)){
-                    $dia7_name = 'DÍA 5';
-                    $dia7_file = '#';
-                }else{
-                    $dia7_name = $value->attributes->MESA_DIA_7->data->attributes->name;
-                    $dia7_file = $value->attributes->MESA_DIA_7->data->attributes->url;
-                    $dia7_clase = 'bi-green';
-                }
+                    if(empty($value->attributes->SEP8)){
+                        $sep8 = '';
+                    }else{
+                        $sep8 = $value->attributes->SEP8;                        
+                    }
+                    if(empty($value->attributes->SEP9)){
+                        $sep9 = '';
+                    }else{
+                        $sep9 = $value->attributes->SEP9;                        
+                    }
+                    if(empty($value->attributes->SEP10)){
+                        $sep10 = '';
+                    }else{
+                        $sep10 = $value->attributes->SEP10;                        
+                    }   
 
-                if(empty($value->attributes->MESA_DIA_8->data->attributes->url)){
-                    $dia8_name = 'DÍA 6';
-                    $dia8_file = '#';
-                }else{
-                    $dia8_name = $value->attributes->MESA_DIA_8->data->attributes->name;
-                    $dia8_file = $value->attributes->MESA_DIA_8->data->attributes->url;
-                    $dia8_clase = 'bi-green';
-                } 
+                    if(empty($value->attributes->MESA_DIA_7->data->attributes->url)){
+                        $dia7_name = 'DÍA 5';
+                        $dia7_file = '#';
+                    }else{
+                        $dia7_name = $value->attributes->MESA_DIA_7->data->attributes->name;
+                        $dia7_file = $value->attributes->MESA_DIA_7->data->attributes->url;
+                        $dia7_clase = 'bi-green';
+                    }
 
-                if(empty($value->attributes->MESA_DIA_9->data->attributes->url)){
-                    $dia9_name = 'DÍA 7';
-                    $dia9_file = '#';
-                }else{
-                    $dia9_name = $value->attributes->MESA_DIA_9->data->attributes->name;
-                    $dia9_file = $value->attributes->MESA_DIA_9->data->attributes->url;
-                    $dia9_clase = 'bi-green';
-                } 
-                
-                if(empty($value->attributes->ENCUESTA)){
-                    $encuesta = '';
+                    if(empty($value->attributes->MESA_DIA_8->data->attributes->url)){
+                        $dia8_name = 'DÍA 6';
+                        $dia8_file = '#';
+                    }else{
+                        $dia8_name = $value->attributes->MESA_DIA_8->data->attributes->name;
+                        $dia8_file = $value->attributes->MESA_DIA_8->data->attributes->url;
+                        $dia8_clase = 'bi-green';
+                    } 
 
-                }else{
-                    $encuesta = $value->attributes->ENCUESTA;
-                } 
-                if(empty($value->attributes->BOOKING)){
-                    $booking = '';
-                }else{
-                    $booking = $value->attributes->BOOKING;
-                } 
-                
-                if(empty($value->attributes->BePrime)){
-                    $BePrime = '';
-                }else{
-                    $BePrime = $value->attributes->BePrime;
-                }
-                if(empty($value->attributes->Syniti)){
-                    $Syniti = '';
-                }else{
-                    $Syniti = $value->attributes->Syniti;
-                }
-                if(empty($value->attributes->RakenDataGroup)){
-                    $RakenDataGroup = '';
-                }else{
-                    $RakenDataGroup = $value->attributes->RakenDataGroup;
-                } 
-                if(empty($value->attributes->Nutanix)){
-                    $Nutanix = '';
-                }else{
-                    $Nutanix = $value->attributes->Nutanix;
-                } 
-                if(empty($value->attributes->Appsell)){
-                    $Appsell = '';
+                    if(empty($value->attributes->MESA_DIA_9->data->attributes->url)){
+                        $dia9_name = 'DÍA 7';
+                        $dia9_file = '#';
+                    }else{
+                        $dia9_name = $value->attributes->MESA_DIA_9->data->attributes->name;
+                        $dia9_file = $value->attributes->MESA_DIA_9->data->attributes->url;
+                        $dia9_clase = 'bi-green';
+                    } 
+                    
+                    if(empty($value->attributes->ENCUESTA)){
+                        $encuesta = '';
 
-                }else{
-                    $Appsell = $value->attributes->Appsell;
-                } 
-                if(empty($value->attributes->C3ntroTelecom)){
-                    $C3ntroTelecom = '';
+                    }else{
+                        $encuesta = $value->attributes->ENCUESTA;
+                    } 
+                    if(empty($value->attributes->BOOKING)){
+                        $booking = '';
+                    }else{
+                        $booking = $value->attributes->BOOKING;
+                    } 
+                    
+                    if(empty($value->attributes->BePrime)){
+                        $BePrime = '';
+                    }else{
+                        $BePrime = $value->attributes->BePrime;
+                    }
+                    if(empty($value->attributes->Syniti)){
+                        $Syniti = '';
+                    }else{
+                        $Syniti = $value->attributes->Syniti;
+                    }
+                    if(empty($value->attributes->RakenDataGroup)){
+                        $RakenDataGroup = '';
+                    }else{
+                        $RakenDataGroup = $value->attributes->RakenDataGroup;
+                    } 
+                    if(empty($value->attributes->Nutanix)){
+                        $Nutanix = '';
+                    }else{
+                        $Nutanix = $value->attributes->Nutanix;
+                    } 
+                    if(empty($value->attributes->Appsell)){
+                        $Appsell = '';
 
-                }else{
-                    $C3ntroTelecom = $value->attributes->C3ntroTelecom;
-                } 
-                if(empty($value->attributes->Equinix)){
-                    $Equinix = '';
+                    }else{
+                        $Appsell = $value->attributes->Appsell;
+                    } 
+                    if(empty($value->attributes->C3ntroTelecom)){
+                        $C3ntroTelecom = '';
 
-                }else{
-                    $Equinix = $value->attributes->Equinix;
-                } 
-                if(empty($value->attributes->Linko)){
-                    $Linko = '';
+                    }else{
+                        $C3ntroTelecom = $value->attributes->C3ntroTelecom;
+                    } 
+                    if(empty($value->attributes->Equinix)){
+                        $Equinix = '';
 
-                }else{
-                    $Linko = $value->attributes->Linko;
-                } 
-                if(empty($value->attributes->NETjer)){
-                    $NETjer = '';
+                    }else{
+                        $Equinix = $value->attributes->Equinix;
+                    } 
+                    if(empty($value->attributes->Linko)){
+                        $Linko = '';
 
-                }else{
-                    $NETjer = $value->attributes->NETjer;
-                } 
-                if(empty($value->attributes->Digital)){
-                    $Digital = '';
+                    }else{
+                        $Linko = $value->attributes->Linko;
+                    } 
+                    if(empty($value->attributes->NETjer)){
+                        $NETjer = '';
 
-                }else{
-                    $Digital = $value->attributes->Digital;
-                } 
-   
-            }               
+                    }else{
+                        $NETjer = $value->attributes->NETjer;
+                    } 
+                    if(empty($value->attributes->Digital)){
+                        $Digital = '';
 
-         
-        return view('layouts.agenda', compact('title','sep7','sep8','sep9','sep10','dia7_file','dia7_name','dia8_file','dia8_name','dia9_file','dia9_name','dia7_clase', 'dia8_clase','dia9_clase','encuesta','booking','BePrime','Syniti','RakenDataGroup','Nutanix','Appsell','C3ntroTelecom','Equinix','Linko','NETjer','Digital'));
+                    }else{
+                        $Digital = $value->attributes->Digital;
+                    } 
+    
+                }               
+            
+                return view('layouts.agenda', compact('title','sep7','sep8','sep9','sep10','dia7_file','dia7_name','dia8_file','dia8_name','dia9_file','dia9_name','dia7_clase', 'dia8_clase','dia9_clase','encuesta','booking','BePrime','Syniti','RakenDataGroup','Nutanix','Appsell','C3ntroTelecom','Equinix','Linko','NETjer','Digital'));
+            }else{
+                return view('layouts.agenda_offline', compact('title'));                
+            }
+
     }
 }
---
+++ resources/views/layouts/agenda_offline.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  <head>
+  <title>{{  $title }}</title>
+
+  <!-- facebook-->
+  <meta property="og:title" content="{{  $title }}"/>
+  <meta property="og:url" content="{{ url()->current() }}" />
+  <meta name="keywords" content="{{  $title }}"/>
+  <meta property="og:image" content="https://cioslatam.com/dash/images/cios/trd1.jpeg">
+  <meta property="og:image:url" content="https://cioslatam.com/dash/images/cios/trd1.jpeg">
+  <meta property="og:image:width" content="300" />
+  <meta property="og:image:height" content="300" />
+
+  <link rel="apple-touch-icon" href="https://cioslatam.com/dash/images/cios/trd1.jpeg">
+  <meta name="apple-mobile-web-app-title" content="{{ $title }}">
+
+
+    @include('layouts.css')
+    <style>
+      @media (max-width: 400px) {
+        #portada {
+            background-image: url(/dash/images/cios/trd1.jpeg) !important;
+            background-size: cover;
+        }
+        .event-item-classic{
+          flex-direction: row-reverse !important;
+        }
+    }
+    .icono_evento{
+          bottom: 20px;
+          width: 60px;
+          left: 20px;
+          position: absolute;
+          transition: .5s ease;
+        }
+    .fs-2{
+      font-size: 2rem !important;
+    }
+    .bi-green{
+      width: 3em;
+      height: 3em;
+      color: #007bff!important;
+    }
+    .bi-red{
+      width: 3em;
+      height: 3em;
+      color: #dc3545!important;
+    }
+    
+    </style>
+  </head>
+  <body>
+  <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="dash/images/cios/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <div class="preloader">
+      <div class="preloader-body">
+        <div class="cssload-container">
+          <div class="cssload-speeding-wheel"></div>
+        </div>
+      </div>
+    </div>
+    <div class="page">
+      <!-- Page Header-->
+      @include('layouts.header')
+      <section id="portada">
+        <div class="parallax-content context-dark"> 
+          
+            <img src="/dash/images/cios/trd1.jpeg" alt="CIO's LATAM Technology Retreat 2024 Hotel Las Brisas" class="img-fluid w-100">
+
+        </div>
+      </section>
+      <section class="section-lg section bg-gray-1">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-10 col-xl-9 text-center wow-outer">
+              <div class="wow slideInLeft">
+                <h3 class="title-decorate title-decorate-center"> <br><H1>En Breve podrás consultar la agenda del evento aquí</H1></h3>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+      <!-- Sidebar -->
+      @include('layouts.footer')
+      <!-- End of Sidebar -->
+    </div>
+    <div class="snackbars" id="form-output-global"></div>
+    @extends('layouts.js')
+  </body>
+</html>
