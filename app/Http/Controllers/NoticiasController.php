15b7ae371ed8c71188f546140e8408440f8bfadc
delfinoruizsalinas <delfinoruizsalinas@hotmail.com>
2026-02-19 13:15:59 -0600

    controlador

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -8,41 +8,72 @@ use Illuminate\Support\Str;
 
 class NoticiasController extends Controller
 {
-    public function index(){
-        $title = "CIO's LATAM - NOTICIAS";
-        //$noticias =""; 
-        // NOTICIAS
-        //$url = 'https://api.rss2json.com/v1/api.json?rss_url=https://expansion.mx/rss/tecnologia';
-        $url = 'apiNoticias.json';
-        $response = file_get_contents($url);
-        $newsData = json_decode($response);
-        $i=0;
-        foreach ($newsData->items as $value) {
-            if($i <=8){
-                $img = "";
-                $converted = strip_tags(Str::substr($value->content, 1, 200));
+    // public function index(){
+    //     $title = "CIO's LATAM - NOTICIAS";
+    //     //$noticias =""; 
+    //     // NOTICIAS
+    //     //$url = 'https://api.rss2json.com/v1/api.json?rss_url=https://expansion.mx/rss/tecnologia';
+    //     $url = 'apiNoticias.json';
+    //     $response = file_get_contents($url);
+    //     $newsData = json_decode($response);
+    //     $i=0;
+    //     foreach ($newsData->items as $value) {
+    //         if($i <=8){
+    //             $img = "";
+    //             $converted = strip_tags(Str::substr($value->content, 1, 200));
 
-                if (strlen($converted)>0) {
-                    if(empty($value->enclosure->link)){
-                        $img = "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg";
-                    }else{
-                        if($value->enclosure->link){
-                            $img = $value->enclosure->link;
-                        }else{
-                            $img = $value->thumbnail;
-                        }
-                    }
+    //             if (strlen($converted)>0) {
+    //                 if(empty($value->enclosure->link)){
+    //                     $img = "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg";
+    //                 }else{
+    //                     if($value->enclosure->link){
+    //                         $img = $value->enclosure->link;
+    //                     }else{
+    //                         $img = $value->thumbnail;
+    //                     }
+    //                 }
     
-                    $dateformat = Carbon::parse($value->pubDate)->format('d-m-Y h:m');
+    //                 $dateformat = Carbon::parse($value->pubDate)->format('d-m-Y h:m');
                    
     
-                    $noticias[] = array('titulo' => $value->title, 'link'=> $value->link,'description'=> $value->description,'img'=>$img, 'fecha' =>$dateformat,'content'=>$converted);
-                    $i++;
-                }
-            }
+    //                 $noticias[] = array('titulo' => $value->title, 'link'=> $value->link,'description'=> $value->description,'img'=>$img, 'fecha' =>$dateformat,'content'=>$converted);
+    //                 $i++;
+    //             }
+    //         }
             
-        }
+    //     }
         
-        return view('layouts.noticias', compact('title','noticias'));
+    //     return view('layouts.noticias', compact('title','noticias'));
+    // }
+    public function index() {
+    $title = "CIO's LATAM - NOTICIAS";
+    $path = public_path('apiNoticias.json');
+
+    if (file_exists($path)) {
+        $response = file_get_contents($path);
+        $newsData = json_decode($response);
+
+        $noticias = [];
+        $i = 0;
+
+        foreach ($newsData->items as $value) {
+            if ($i <= 8) { // Mantenemos tu límite original
+                // IMPORTANTE: Usamos los nombres que vienen en tu nuevo JSON
+                $noticias[] = array(
+                    'titulo'      => $value->titulo,
+                    'link'        => $value->link,
+                    'description' => $value->description,
+                    'img'         => $value->img ?: 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-on-blue-background-free-vector.jpg',
+                    'fecha'       => $value->fecha, // Ya viene formateada del script
+                    'content'     => $value->content
+                );
+            }
+            $i++;
+        }
+    } else {
+        $noticias = [];
     }
+
+    return view('layouts.noticias', compact('title', 'noticias'));
+}
 }
