a4f05c9c3e114c93ce97d84d4b39750b128c53c9
Delfino Ruiz Salinas <delfinoruizsalinas@gmail.com>
2026-07-30 01:38:15 -0600

    slider

--- resources/views/layouts/partner_slider.blade.php
+++ resources/views/layouts/partner_slider.blade.php
@@ -3,7 +3,7 @@
         font-size: 22px !important;
     }
 </style>
-<section class="section-lg bg-gray-200 text-center">
+<section class="section-lg bg-gray-200 text-center partner-slider-section">
     <div class="container">
         <div class="wow-outer">
         <div class="wow slideInDown">
@@ -11,14 +11,36 @@
         </div>
         </div>
         <!-- Owl Carousel-->
-        <div class="owl-carousel owl-dots-secondary dots-offset-lg" data-items="7" data-autoplay="true" data-dots="true" data-nav="false" data-stage-padding="10" data-loop="true" data-margin="10">
+        <div class="owl-carousel owl-dots-secondary dots-offset-lg" data-items="1" data-sm-items="2" data-md-items="4" data-lg-items="6" data-autoplay="true" data-dots="true" data-nav="false" data-stage-padding="10" data-loop="true" data-margin="10">
 
             @foreach($partner_slider as $partners)
             
             <a class="box-sponsor box-sponsor-modern wow-outer" href="{{ $partners['link'] }}" target="_blank">
-                <div class="wow fadeInUp"> <img src="{{ $partners['imagen'] }}" alt="" style="height: 121px;" /> </div>
+                <div class="wow fadeInUp"> <img src="{{ $partners['imagen'] }}" alt="" class="partner-logo" /> </div>
             </a>
             @endforeach
         </div>
     </div>
 </section>
+
+<style>
+.partner-slider-section .owl-item {
+    display: flex;
+    justify-content: center;
+}
+.partner-slider-section .box-sponsor {
+    width: 100%;
+    max-width: 220px;
+}
+.partner-slider-section .box-sponsor .wow {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.partner-slider-section .partner-logo {
+    max-width: 100%;
+    max-height: 120px;
+    height: auto;
+    width: auto;
+}
+</style>
